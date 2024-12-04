0979085f549a721abc9b55f57bc4727b1e93db82
Dan Moinescu <dan@moinescu.net>
2024-12-04 20:10:34 +0200

    Read arguments from the command line

--- src/stl-threads-demo.cpp
+++ src/stl-threads-demo.cpp
@@ -1,12 +1,30 @@
 #include "stl-threads-demo.hpp"
+#include <cstdlib>
 
 
-int main()
+static void usage(const char *me)
 {
-    const int num_work_units = 499; //TODO: take from argv
+    fprintf(stderr, "Usage: %s num_workers work_units\n", me);
+}
+
+
+int main(int argc, const char* const argv[])
+{
+    if(argc<3)
+    {
+        usage(argv[0]);
+        return 1;
+    }
+    const int num_workers = atoi(argv[1]);
+    const int total_work_units = atoi(argv[2]);
+    if(num_workers <= 0 || total_work_units <= 0)
+    {
+        usage(argv[0]);
+        return 1;
+    }
 
     Dispatcher dispatcher = {
-        num_work_units
+        total_work_units
     };
     std::thread dispatcher_thread([&dispatcher] { dispatcher.run(); });
 
@@ -14,11 +32,10 @@ int main()
     while(!dispatcher.is_ready()) {}
 
     Synchronized<std::vector<long>> global_results;
-    global_results->reserve(num_work_units);
-#define NUM_WORKERS 4
+    global_results->reserve(total_work_units);
     std::vector<std::thread> workers;
-    workers.reserve(NUM_WORKERS);
-    for(int wid=1; wid <= NUM_WORKERS; ++wid)
+    workers.reserve(num_workers);
+    for(int wid=1; wid <= num_workers; ++wid)
     {
         workers.emplace_back([&, wid]
             {
